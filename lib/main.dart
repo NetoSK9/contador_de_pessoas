8a400795f589fbcf1c303be3bd46ce1ee79f45f7
J.B. Neto <netobatista090@gmail.com>
2021-08-11 12:07:17 -0300

    A aplicação está modificando a tela e mostra

--- lib/main.dart
+++ lib/main.dart
@@ -3,51 +3,93 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
       title: "Contador de Pessoas",
-      home: Stack(
-        children: [
-          Image.asset(
-            "images/restaurant.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Pessoas: 0",
-                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                children: <Widget> [
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: TextButton(
-                      onPressed: (){},
-                      child: Text("+1", style: TextStyle(fontSize: 40, color: Colors.white)),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: TextButton(
-                      onPressed: (){},
-                      child: Text("-1", style: TextStyle(fontSize: 40, color: Colors.white)),
-                    ),
-                  ),
-                ],
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-              Text(
-                "Pode Entrar!",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+      home: Home(),
   )
   );
+}
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people=0;
+  String _infoText="Pode Entrar !";
+
+  void _changePeople(int delta){
+    setState(() {
+      if(!(_people>=10 && delta>0)){
+        _people+=delta;
+      }
+
+       if(_people<0){
+        _infoText="Estamos no mundo invertido!";
+      }
+      else if (_people<10){
+        _infoText="Pode Entrar !";
+      }
+      else{
+        _infoText="Restaurante cheio. Aguarde!";
+      }
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_people",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              children: <Widget> [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextButton(
+                    onPressed: (){
+                      _changePeople(1);
+                    },
+                    child: Text("+1", style: TextStyle(fontSize: 40, color: Colors.white)),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextButton(
+                    onPressed: (){
+                      _changePeople(-1);
+                    },
+                    child: Text("-1", style: TextStyle(fontSize: 40, color: Colors.white)),
+                  ),
+                ),
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
